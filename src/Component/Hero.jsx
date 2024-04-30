3e1c3bd061bb6182408d19baa66b43b883898e56
Ravi <bhardwajravi2025@gmail.com>
2024-05-01 02:33:19 +0530

    portfolio

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -8,10 +8,10 @@ import { Link } from 'react-scroll';
 export const Hero = () => {
 
     const onButtonClick = () => {
-        const pdfUrl = "0001.pdf";
+        const pdfUrl = "RAVI KUMAR.pdf";
         const link = document.createElement("a");
         link.href = pdfUrl;
-        link.download = "document.pdf"; // specify the filename
+        link.download = "RAVI.pdf"; // specify the filename
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);

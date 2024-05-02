2e4d429886a3fd1e0496316a1c460a720df187dd
Ravi <bhardwajravi2025@gmail.com>
2024-05-02 23:36:35 +0530

    portfolio

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -5,44 +6,41 @@ const multer = require("multer");
 const upload = multer();
 app.use(upload.none());
 app.use(express.json());
-require("dotenv").config();
+// app.use(cors());
+app.use(
+ cors({
+ origin: "http://localhost:3000",
+ methods: ["POST"],
+ allowedHeaders: ["Content-Type", "Authorization"],
+ })
+);
+
 const PORT = process.env.PORT || 4000;
 
-
 const route = require("./Routes/routes");
-app.use("/api/v1",route);
+app.use("/api/v1", route);
 
-
-/*  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Header','*');
-    
-    next();
-  }); */
+/* app.use((req, res, next) => {
+ res.header('Access-Control-Allow-Origin', '*');
+ res.header('Access-Control-Allow-Header','*');
+ 
+ next();
+ }); */
 
 /* app.use(cors({
-    origin:"*",
-    credentials:true,
-    allowedHeaders : ["Content-Type", "Authorization"]
-    
-})) */ 
-
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-    allowedHeaders: ["Accept", "Accept-Language", "Content-Language", "Content-Type"],
-    methods: ["POST"]
-}));
-
-
+ origin:"*",
+ credentials:true,
+ allowedHeaders : ["Content-Type", "Authorization"]
+ 
+})) */
 
 const dbConnect = require("./Config/database");
 dbConnect();
 
-app.listen(PORT,()=>{
-    console.log(`App is listening at ${PORT}`);
-})
+app.listen(PORT, () => {
+ console.log(`App is listening at ${PORT}`);
+});
 
-app.get("/",(req,res)=>{
-    res.send("Your Server is up and running");
-})
+app.get("/", (req, res) => {
+ res.send("Your Server is up and running");
+});

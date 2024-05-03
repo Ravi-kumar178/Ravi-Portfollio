925cec9dd1b08f43e82bbd99d5d32d2d44f969a9
Ravi <bhardwajravi2025@gmail.com>
2024-05-03 17:06:43 +0530

    portfolio

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
  cors({
- origin: "http://localhost:3000",
+ origin: "*",
  methods: ["POST"],
  allowedHeaders: ["Content-Type", "Authorization"],
  })
